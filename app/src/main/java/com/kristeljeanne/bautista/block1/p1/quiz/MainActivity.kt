39d062100eed8b2157c50a4d8e63799c3fd27d62
KristelJeanneB <krid.bautista.up@phinmaed.com>
2023-12-07 11:30:36 +0800

    log in

--- app/src/main/java/com/kristeljeanne/bautista/block1/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/kristeljeanne/bautista/block1/p1/quiz/MainActivity.kt
@@ -15,18 +15,10 @@ import com.kristeljeanne.bautista.block1.p1.quiz.ui.theme.KristelJeanneBautistaB
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            KristelJeanneBautistaBlock1QuizTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        (layoutInflater)
+        setContentView(R.layout.activity)
     }
+
 }
 
 @Composable
